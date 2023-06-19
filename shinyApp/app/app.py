33fde9169240873472195ee6ef44363c8b818312
Yoann <yoann.chamillard@gmail.com>
2023-06-19 16:32:25 +0200

    App livrable soutenance

--- shinyApp/app/app.py
+++ shinyApp/app/app.py
@@ -158,17 +158,6 @@ plt.show()
 
         if graphReady.get() != True: #Show default graph
             return
-            # Data for plotting
-            t = np.arange(0.0, 2.0, 0.01)
-            s = 1 + np.sin(2 * np.pi * t)
-
-            fig, ax = plt.subplots()
-            ax.plot(t, s)
-
-            ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-                title='About as simple as it gets, folks')
-            ax.grid()
-            return fig
         else:
             
             time = graphSers[0]
